1d72aafa36a89562b113002d3750723cc8a698de
pasterntt <support@yenda.de>
2014-07-04 22:41:44 +0200

    Added Smarty
    Changelog:
    Added Smarty

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php
-    require_once('classes/generateForums.class.php');
-    $Forum = new Forum();
-    print_r($Forum->returnForums());
-    echo '<br>';
-    foreach($Forum->returnForums() as $data){
-        echo '<h1>'.$data['name'].'</h2>';
-        echo '<p>'.$data['description'].'</p>';
-    }
-?>
+/**
+ * Example Application
+ *
+ * @package Example-application
+ */
+
+require 'lib/smarty/libs/Smarty.class.php';
+
+$smarty = new Smarty;
+
+$smarty->setTemplateDir('templates/');
+$smarty->setCompileDir('lib/smarty/templates_c/');
+$smarty->setConfigDir('lib/smarty/configs/');
+$smarty->setCacheDir('lib/smarty/cache/');
+
+//$smarty->force_compile = true;
+$smarty->debugging = true;
+$smarty->caching = true;
+$smarty->cache_lifetime = 120;
+
+$smarty->assign("Name", "Fred Irving Johnathan Bradley Peppergill", true);
+$smarty->assign("FirstName", array("John", "Mary", "James", "Henry"));
+$smarty->assign("LastName", array("Doe", "Smith", "Johnson", "Case"));
+$smarty->assign("Class", array(array("A", "B", "C", "D"), array("E", "F", "G", "H"),
+                               array("I", "J", "K", "L"), array("M", "N", "O", "P")));
+
+$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
+                                  array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
+
+$smarty->assign("option_values", array("NY", "NE", "KS", "IA", "OK", "TX"));
+$smarty->assign("option_output", array("New York", "Nebraska", "Kansas", "Iowa", "Oklahoma", "Texas"));
+$smarty->assign("option_selected", "NE");
+
+$smarty->display('index.tpl');
